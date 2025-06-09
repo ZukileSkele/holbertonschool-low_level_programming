fce5504583fb71747e8e061072b46fac4281a08c
ZukileSkele <ZSkele@singular.co.za>
2025-06-09 14:21:46 +0000

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point of the program
+ *
+ *Description: Writing my first C program, but I love JavaScript, sad.
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
