796c3dad2d177d8f1a02f0d3d189055ef281a250
ZukileSkele <ZSkele@singular.co.za>
2025-06-10 13:01:24 +0000

    fizz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints fizz buzz game;
+ * Return: Retuens fizz buzz 1 - 100
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
